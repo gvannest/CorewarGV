2f37b4aad7e6e8d81ebd8ab75fc23e81ad696233
Martin SICOT <msicot@e1r7p2.42.fr>
2018-06-14 10:23:52 +0200

    test

--- asm/src/main.c
+++ asm/src/main.c
@@ -6,7 +6,7 @@
 /*   By: msicot <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/14 09:22:10 by msicot            #+#    #+#             */
-/*   Updated: 2018/06/14 10:11:18 by msicot           ###   ########.fr       */
+/*   Updated: 2018/06/14 10:19:14 by msicot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@ int	main(int argc, char **argv)
 	int fp;
 
 	if (argc < 2 || argc > 2 || argv[1] == NULL)
-		perror("Error: ");;
+		perror("Error: ");
+	//open
 	fp = open(argv[1], O_RDONLY);
 	if (fp < 0)
 		perror("Error: ");
