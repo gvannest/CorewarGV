fdfbc20c3f8833473ec9f6c07b2a8ee557d60617
Martin SICOT <msicot@e1r7p2.42.fr>
2018-06-14 10:17:35 +0200

    Dossier asm + Makefile

---
+++ asm/includes/asm.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   asm.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: srossi <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/13 17:53:35 by srossi            #+#    #+#             */
+/*   Updated: 2018/06/14 10:00:12 by msicot           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ASM_H
+# define ASM_H
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdio.h>
+void	ft_error(int a);
+/*
+typedef struct s_label
+{
+	char 	*name;
+	struct	*s_instruction;
+	int		address;
+
+}
+
+typedef struct s_asm
+{
+	char 	*tab;
+	struct 	*label;
+}			  t_asm;
+
+*/
+#endif
