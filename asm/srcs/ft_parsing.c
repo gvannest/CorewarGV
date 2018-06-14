0223312900c224fa27358d41cb9ba88c0f55a9a0
Martin SICOT <msicot@e1r7p2.42.fr>
2018-06-14 17:04:59 +0200

    parsing

--- asm/srcs/ft_parsing.c
+++ asm/srcs/ft_parsing.c
@@ -6,7 +6,7 @@
 /*   By: msicot <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/14 13:54:26 by msicot            #+#    #+#             */
-/*   Updated: 2018/06/14 14:59:05 by msicot           ###   ########.fr       */
+/*   Updated: 2018/06/14 16:49:46 by msicot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ void	ft_gnl(int fp)
 	while (get_next_line(fp, & line) > 0)
 	{
 		ft_printf("%s\n", line);
+		ft_strdel(&line);
+
 	}
 	//save
 	//parse
