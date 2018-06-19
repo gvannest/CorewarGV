afc12f39465e4e9638d82804a974cd37e65dc112
Savinien ROSSI <srossi@e1r8p3.42.fr>
2018-06-19 14:19:04 +0200

    update ft_display

--- corewar_project/asm/srcs/ft_display.c
+++ corewar_project/asm/srcs/ft_display.c
@@ -6,41 +6,41 @@
 /*   By: srossi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/18 13:30:10 by srossi            #+#    #+#             */
-/*   Updated: 2018/06/18 15:39:57 by srossi           ###   ########.fr       */
+/*   Updated: 2018/06/19 14:18:53 by srossi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "asm.h"
 #include "libft.h"
 
-void ft_display_param(t_param param, int param_id)
+void ft_display_param(t_param *param, int param_id)
 {
-	ft_printf("Parametre %d :\n     Type : %s (%i)\n     Value : %s (%b)\n     Label : %i\n", param_id, param.type_name, param.type, param.str_value, param.int_value, param.f_label);	
+	ft_printf("Parametre %d :\n     Type : %s (%i)\n     Value : %s (%b)\n     Label : %i\n", param_id, param->type_name, param->type, param->str_value, param->int_value, param->f_label);	
 }
 
-void	ft_display_instruction(t_instruction instruction)
+void	ft_display_instruction(t_instruction *instruction)
 {
-	ft_printf("Instruction :\n  Nom : %s\n  OCP : %c\n  Opcode : %c\n", instruction.name, instruction.ocp, instruction.opcode);
-	ft_display_param(instruction.param1, 1);
-	ft_display_param(instruction.param2, 2);
-	ft_display_param(instruction.param3, 3);
+	ft_printf("Instruction :\n  Nom : %s\n  OCP : %c\n  Opcode : %c\n", instruction->name, instruction->ocp, instruction->opcode);
+	ft_display_param(&instruction->param1, 1);
+	ft_display_param(&instruction->param2, 2);
+	ft_display_param(&instruction->param3, 3);
 }
 
-void	ft_display_label(t_label label)
+void	ft_display_label(t_label *label)
 {
-	ft_printf("Label :\n Nom : %s\n", lanel.name);
-	ft_display_instruction(label.instruction);
+	ft_printf("Label :\n Nom : %s\n", label->name);
+	ft_display_instruction(label->instruction);
 }
 
-void	ft_display_asm(t_asm asm)
+void	ft_display_asm(t_asm *sasm)
 {
 	int i;
 	i = 0;
 	ft_printf("ASM :\n");
-	while (asm.tab[i])
+	while (sasm->tab[i])
 	{
-		ft_printf("$c ", asm.tab[i]);
+		ft_printf("$c ", sasm->tab[i]);
 		i++;
 	}
-	ft_display_label(asm.label);
+	ft_display_label(sasm->label);
 }
