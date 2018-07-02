801872fa99ffae79d04a723e96d42e4c9e480d72
Martin S <Lock-Tile@pc42.home>
2018-07-02 17:14:18 +0200

    spliting operators

--- corewar_project/asm/srcs/main.c
+++ corewar_project/asm/srcs/main.c
@@ -42,7 +42,8 @@ int	main(int argc, char **argv)
 	(void) argv;
 
 	init_info(&info);
-//	ft_test_params();
+/*	ft_test_params();
+	ft_printf("SAVINEIEN SEGF\n");
 	ft_token_add(&info, "%:live"); //remplacer par l'arg de Martin
 	ft_token_add(&info, "2"); //remplacer par l'arg de Martin
 	ft_token_add(&info, "sti"); //remplacer par l'arg de Martin
@@ -51,12 +52,17 @@ int	main(int argc, char **argv)
 	ft_token_add(&info, "%1"); //remplacer par l'arg de Martin
 	ft_token_display_all(info.atoken);
 //	ft_token_display(info.atoken);
+//	*/
+	ft_printf("SAVINEIEN SEGF 2\n");
 	if (argc < 2 || argc > 2 || argv[1] == NULL)
 		perror("Error");
 	else if ((info.fp = ft_open(argv[1])) == 0)
 		return (0);
 	else
+	{
+		printf("ft_gnl\n");
 		ft_gnl(&info);
+	}
 	//char *line = ft_strdup("tird:    sti    r10,%:fin,r3");
 //	ft_split_line(line);
 	return (0);
