464904c05dc759152b83b22a7c33d656fafe1b4d
gvannest <gautier.vanneste42@gmail.com>
2018-07-26 17:16:39 +0200

    gv winner

--- corewar_project/vm/srcs/ft_game.c
+++ corewar_project/vm/srcs/ft_game.c
@@ -6,16 +6,60 @@
 /*   By: gvannest <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/09 13:59:16 by gvannest          #+#    #+#             */
-/*   Updated: 2018/07/25 13:54:45 by gvannest         ###   ########.fr       */
+/*   Updated: 2018/07/26 17:13:56 by gvannest         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
 
-void		ft_game(t_arena *arena, t_corvisu *visual, char v)
+static void	ft_winner(t_player *tab_pyr, int nb_pyrs, char v, int winner)
 {
+	int i;
+
+	i = 0;
+	(void)v;
+	//if (v == 1)
+	//ft_visual_winner();
+	//else
+	if (winner == 0)
+		ft_printf("You forgot to say live. You Morons...\n");
+	else
+	{
+		winner = nb_pyrs == 1 ? tab_pyr[0].pyr_nbr : winner;
+		{
+			while (i < nb_pyrs)
+			{
+				if (tab_pyr[i].pyr_nbr == winner)
+					ft_printf("Player %d, '%s' , has won the game!\n", winner, tab_pyr[i].pyr_name);
+				i++;
+			}
+		}
+	}
+}
+
+static void	ft_intro_contestants(t_player *tab_pyr, t_corvisu *visual, int nb_pyrs, char v)
+{
+	int i;
+
+	i = 0;
 	if (v == 1)
 		ft_init_visual(visual);
+	else
+	{
+		ft_printf("The champions aaaaaaare ....\n");
+		while (i < nb_pyrs)
+		{
+			ft_printf("Player %d, weighing %d bytes :\n", tab_pyr[i].pyr_nbr, tab_pyr[i].nb_inst);
+			ft_printf("     Name : %s\n", tab_pyr[i].pyr_name);
+			ft_printf("     Comment : %s\n\n", tab_pyr[i].comment);
+			i++;
+		}
+	}
+}
+
+void		ft_game(t_arena *arena, t_corvisu *visual, char v)
+{
+	ft_intro_contestants(arena->tab_pyr, visual, arena->nb_pyrs, v);
 	while (arena->nb_live_proc > 0 && arena->cycle_to_die > 0)
 	{
 		while (arena->nb_cycle_current < arena->cycle_to_die)
@@ -27,11 +71,11 @@ void		ft_game(t_arena *arena, t_corvisu *visual, char v)
 				ft_dump_mem(arena);
 			arena->nb_cycle++;
 			arena->nb_cycle_current++;
-
 		}
 		ft_check_cycle(arena);
 		ft_reinit_cycle(arena, arena->list_proc);
 	}
+	ft_winner(arena->tab_pyr, arena->nb_pyrs, v, arena->last_live_pyr);
 	getch();
 	endwin();
 }
