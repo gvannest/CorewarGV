b72046c2457e3165c11059fdbe21c261c7d7e936
Martin <msicot@student.42.fr>
2018-08-03 15:17:35 +0200

    ok

--- corewar_project/assembler/test_asm/test_output_asm.sh
+++ corewar_project/assembler/test_asm/test_output_asm.sh
@@ -1,29 +1,41 @@
 #!/bin/bash
 clear && printf '\e[3J'
+make -C ../
+rm asm
 cp ../asm .
+#Creation des fichiers de travail
 output="difference.txt"
-path="valid/basic_test/"
+path="valid/"
+origin="origin.txt"
+asmf="asm.txt"
+
 list_file=`find $path -type f -name "*.s"`
 rm $output 2> /dev/null || true
 echo "\033[0;35mChecking unvalid champs\n\033[0m"
 for file in $list_file
 do
+	rm $origin $asm 2> /dev/null || true
 	str=$((basename $file) | awk '{print toupper($0)}')
-	echo "$str" >> $output
-	#remove les anciens log
-	#rm $origin $asm 2> /dev/null || true
-
-	./original_asm $file >> $output
+	./original_asm $file >> $origin
 	s=$file
 	s=${s##*/}
 	s=${s%.*}
 	t="test1"
-	mv $path$s.cor $path$t.cor
-	./asm $file >> $output
-	echo "DIFF COREWAR FILE" >> $output
-	diff $path$s.cor $path$t.cor >> $output
+	mv $path$s.cor $path$t.cor 2> /dev/null || true
+	./asm $file >> $asmf
+	#Retrieve first line of each output and stock it into var
+	s1=$(head -1 $origin)
+	s2=$(head -1 $asmf)
+	if [ "$s1" == "$s2" ]; then
+		echo "$str\033[1;32m OK\033[0m\n" >> $output
+	else
+		echo "\033[1;32m$str:\033[0m\033[1;31m ERROR\n$(head -1 $file)\n\033[1;34mExpected oupout:\033[0m" >> $output
+		echo "$s2" >> $output
+		echo "\033[1;34mYour output:\033[0m" >> $output
+		echo "$s1" >> $output
+		echo "" >> $output
+	fi
+	diff $path$s.cor $path$t.cor >> $output 2> /dev/null || true
 done
-
-#rm useless files
-rm  -r $path*.cor
+rm  -r $path*.cor $origin $asm
 echo "\nThanks for using that script, good luck ;)"
